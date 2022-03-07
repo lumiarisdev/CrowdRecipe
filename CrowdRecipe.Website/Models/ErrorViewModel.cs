3a20370af3ca5b9f63f5626cafbe91fe65d50067
William Ziegler <ziegw1@gmail.com>
2022-03-07 15:50:25 -0600

    Add project files.

---
+++ CrowdRecipe.Website/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CrowdRecipe.Website.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
